cce5546cf99b775caf5825e10fb228cacbed8f5f
borracciaBlu <marco.asdman@gmail.com>
2020-04-07 20:24:31 +1000

    #DHEV-04 : add(test) : Add test with tape

---
+++ test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var test = require('tape');
+var {onClick, onFocus, onBlur, onKeyDown, onKeyUp, generateEventFn} = require('./');
+
+function fireEvent(eventName) {
+    var event = new Event(eventName);
+    document.body.dispatchEvent(event);
+}
+
+test('when interact with element, check for event', function (t) {
+    var stub = 1;
+
+    t.equal(stub, 1);
+
+    onClick(document.body, () => stub = 'click');
+    fireEvent('click');
+
+    t.equal(stub, 'click');
+
+    onFocus(document.body, () => stub = 'focus');
+    fireEvent('focus');
+
+    t.equal(stub, 'focus')
+
+    onBlur(document.body, () => stub = 'blur');
+    fireEvent('blur');
+
+    t.equal(stub, 'blur');
+
+    onKeyDown(document.body, () => stub = 'keydown');
+    fireEvent('keydown');
+
+    t.equal(stub, 'keydown');
+
+    onKeyUp(document.body, () => stub = 'keyup');
+    fireEvent('keyup');
+
+    t.equal(stub, 'keyup');
+
+    var onMouseEnter = generateEventFn('mouseenter');
+
+    onMouseEnter(document.body, () => stub = 'mouseenter');
+    fireEvent('mouseenter');
+
+    t.equal(stub, 'mouseenter');
+
+    t.end();
+});
+
+test('when interact with intruder', function (t) {
+    var stub = 1;
+
+    onKeyUp(null, () => stub = 'keyup');
+    onKeyUp([null], () => stub = 'keyup');
+    onKeyUp(undefined, () => stub = 'keyup');
+    onKeyUp([undefined], () => stub = 'keyup');
+    onKeyUp({}, () => stub = 'keyup');
+    onKeyUp([{}], () => stub = 'keyup');
+
+    t.end();
+});
